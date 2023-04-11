f8a7e8637d3d219ca1bbcefb0e739d8fc3cca47e
Lapsa09 <agusdinar96@gmail.com>
2023-04-10 21:25:32 -0300

    trying to fix expo font error

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,15 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
     plugins: [
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true,
+        },
+      ],
+      ['@babel/plugin-proposal-optional-catch-binding'],
       [
         'module:react-native-dotenv',
         {
